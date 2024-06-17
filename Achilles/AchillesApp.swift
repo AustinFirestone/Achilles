d92c8962368027e13d5f097a5e14644dd0b64467
Austin Firestone <austinfirestone@outlook.com>
2024-06-17 14:40:58 -0500

    Initial Commit

---
+++ Achilles/AchillesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AchillesApp.swift
+//  Achilles
+//
+//  Created by Austin Firestone on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AchillesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
